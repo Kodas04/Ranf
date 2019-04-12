c2d36c4d850dce5d99634c28e410ca49a6808c25
Piet <n/a>
2019-04-12 20:37:27 +0200

    Basics

--- src/ranf/Main.java
+++ src/ranf/Main.java
@@ -5,16 +5,5 @@ public class Main {
 	public static void Main(String[] arg) {
 		
 	}
-	
-<<<<<<< HEAD
-	public void s() {
-=======
-	public void s(es funktioniert)
-	{
->>>>>>> c4c244f455d835207d50a72db7788ec9485ea04e
-		
-		
-		
-	}
 
 }
