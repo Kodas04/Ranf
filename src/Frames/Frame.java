0c556c931992739373b9a1b645170d19ef23c12a
Your Name <you@example.com>
2019-04-12 21:36:25 +0200

    lol

--- src/Frames/Frame.java
+++ src/Frames/Frame.java
@@ -2,27 +2,13 @@ package Frames;
 
 import javax.swing.JFrame;
 
-public class Frame {
-	
-	JFrame frame;
-	
-	String title;
-	
-	int width, height;
-	
-	Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-	int screen_width =
-	
-	public void Frame() {
-		
-		frame("Frame, ");
+public class Frame extends JFrame{
 
 	public void Frame(String title, int width, int height, boolean visible) {
 		
-		JFrame frame = new JFrame();
-		frame.setTitle(title);
-		frame.setSize(width, height);
-		frame.setVisible(visible);
+		this.setTitle(title);
+		this.setSize(width, height);
+		this.setVisible(true);
 		
 		
 	}
