78048709ee0071270b7f189996e5fb0a66eb3cec
Piet <n/a>
2019-04-12 21:38:12 +0200

    lol
    9m. Menschen können nicht lesen und schreiben

--- src/Frames/Frame.java
+++ src/Frames/Frame.java
@@ -2,15 +2,29 @@ package Frames;
 
 import javax.swing.JFrame;
 
+import gfx.View;
+
 public class Frame extends JFrame{
 
+	public View view;
+	
 	public void Frame(String title, int width, int height, boolean visible) {
 		
 		this.setTitle(title);
 		this.setSize(width, height);
 		this.setVisible(true);
-		
-		
 	}
+
+	public View getView()
+	{
+		return view;
+	}
+
+	public void setView(View view)
+	{
+		this.view = view;
+	}
+	
+	
 
 }
